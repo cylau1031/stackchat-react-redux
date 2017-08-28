232b66f23b512715f4197e85755d6d77ee7d175f
cylau1031 <cylau1031@gmail.com>
2017-08-28 11:43:20 -0400

    finish channellist

---
+++ client/components/ChannelList.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import store from '../store';
+import {connect} from 'react-redux'
+
+// These values are all hardcoded...for now!
+// Soon, we'll fetch them from the server!
+const RANDOM_CHANNEL = '/channels/1';
+const GENERAL_CHANNEL = '/channels/2';
+const DOGS_CHANNEL = '/channels/3';
+const LUNCH_CHANNEL = '/channels/4';
+
+class ChannelList extends Component {
+
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    const { messages, channels } = this.props;
+
+    return (
+      <ul>
+          {channels.map( channel => {
+            return (
+              <li key={channel.id}>
+                <NavLink to={`/channels/${channel.id}`} activeClassName="active">
+                <span>#{channel.name}</span>
+                <span className="badge">{ messages.filter(message => message.channelId === channel.id).length }</span>
+                </NavLink>
+              </li>
+            )
+          })}
+        <li>
+        <NavLink to='/new-channel'>Create a channel...</NavLink>
+        </li>
+      </ul>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    channels: state.channels,
+    messages: state.messages
+  }
+}
+
+
+const ChannelListContainer = connect(mapStateToProps)(ChannelList)
+
+export default ChannelListContainer
---
+++ client/components/Main.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import Navbar from './Navbar';
+import MessagesList from './MessagesList';
+import store, { fetchMessages, fetchChannels } from '../store';
+
+
+export default class Main extends Component {
+
+  componentDidMount () {
+
+    store.dispatch(fetchMessages());
+    store.dispatch(fetchChannels())
+  }
+
+  render () {
+    return (
+      <div>
+        <Sidebar />
+        <Navbar />
+        <main>
+          <Switch>
+            <Route path="/channels/:channelId" component={MessagesList} />
+            <Redirect to="/channels/1" />
+          </Switch>
+        </main>
+      </div>
+    );
+  }
+}
